f47376c69518778185dbddede49b441f3e527d74
Adam Smith <adam_smith3@comcast.com>
2021-03-19 10:02:57 -0400

    Add fees

--- aggressive.py
+++ aggressive.py
@@ -7,7 +7,9 @@ def aggressive_trade(exchange, current_value, buys, sells, portfolio):
     portfolio = commons.zero_out_portfolio(portfolio)
     num_buys = len(buys)
     # divide value among buys
-    price_per_buy = current_value / num_buys
+    price_per_buy_before_fees = current_value / num_buys
+    fees = price_per_buy_before_fees * .001
+    price_per_buy = price_per_buy_before_fees - fees
     for buy in buys:
         try:
             j = json.dumps(exchange.fetchTicker(buy + "/USD"), indent=4, sort_keys=True)
@@ -24,7 +26,9 @@ def aggressive_backtest_trade(buy_prices, current_value, buys, sells, portfolio)
     portfolio = commons.zero_out_portfolio(portfolio)
     num_buys = len(buys)
     # divide value among buys
-    price_per_buy = current_value / num_buys
+    price_per_buy_before_fees = current_value / num_buys
+    fees = price_per_buy_before_fees * .001
+    price_per_buy = price_per_buy_before_fees - fees
     for buy in buys:
         portfolio[buy] = float(price_per_buy) / float(buy_prices[buy])
         print("buy " + str(portfolio[buy]) + " shares of " + buy + " for " + str(price_per_buy) + " at $" + str(buy_prices[buy]))
