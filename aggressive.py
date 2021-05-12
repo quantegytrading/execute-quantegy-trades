88defb6e99e85eb2ab55cb9674ff77eb466d9e3d
Adam Smith <adam_smith3@comcast.com>
2021-05-12 16:24:34 -0400

    fix prod conditional

--- aggressive.py
+++ aggressive.py
@@ -48,6 +48,7 @@ def main(event, context):
     maker_taker = os.environ['maker_taker']
     trade_style = os.environ['trade_style']
     prod = os.environ['prod']
+    print("Prod: " + prod)
     if prod == "True":
         commons.go_live(event, aggressive_trade, aggressive_backtest_trade, maker_taker, trade_style)
     else:
