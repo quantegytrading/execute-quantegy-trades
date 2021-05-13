b83bcf644449bab3a6c3a58665a8128c46a134d6
Adam Smith <adam_smith3@comcast.com>
2021-05-13 15:43:54 -0400

    attempt to recapture fee

--- commons.py
+++ commons.py
@@ -123,9 +123,10 @@ def get_backtest_portfolio_value(price_guide, portfolio):
 def get_current_live_portfolio_value(exchange, portfolio) -> str:
     curr_val = 0
     for symbol in portfolio:
-        ticker = exchange.fetchTicker(symbol + "/USDT")
-        count = portfolio.get(symbol)
-        curr_val = curr_val + (float(count) * float(ticker.get('ask')))
+        if symbol != 'USD':
+            ticker = exchange.fetchTicker(symbol + "/USD")
+            count = portfolio.get(symbol)
+            curr_val = curr_val + (float(count) * float(ticker.get('ask')))
     return str(curr_val)
 
 
@@ -299,7 +300,7 @@ def go_live(event, trade_fn, backtest_trade_fn, maker_taker, trade_style):
                     ## This block tries to re-capture the fee by putting a limit order minus the fee
                     fee_per_share = (float(price) * float(count) * 0.075)/float(count)
                     price = float(price) - fee_per_share
-                    ## 
+                    ##
                     order = exchange.createLimitBuyOrder(pair, float(count), price)
                     print("** MAKER ORDER **")
                     print(order)
