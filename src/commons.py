6e93aed8d0a88e0e6eb209f99896d21ec5c7f0f3
Adam Smith <agsmith11@gmail.com>
2025-04-14 12:45:34 -0400

    Update commons.py

--- src/commons.py
+++ src/commons.py
@@ -281,7 +281,7 @@ def go_slack(event, trade_fn):
     buys_prices = sym_price(buys, prices)
     sells_prices = sym_price(sells, prices)
     d = datetime.now().strftime("%m/%d %H:%M:")
-    slack_post(f'```{d}\nbuys:\n{'\n'.join(map(str, buys_prices))}```\n```sells:\n{'\n'.join(map(str, sells_prices))}```')
+    slack_post(f'```{d}\nbuys:\n{"\n".join(map(str, buys_prices))}```\n```sells:\n{"\n".join(map(str, sells_prices))}```')
     time.sleep(1)
     # for buy in buys_prices:
     #     slack_post(str(buy))
